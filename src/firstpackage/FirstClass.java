4d4ef2b345e51882dfe02e0918960fc9fcc97711
Meryem Togan <meryemtogan6@gmail.com>
2020-08-15 18:17:00 +0200

    last commit from local master branch

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,5 +6,8 @@ public class FirstClass {
 		System.out.println("Our first commit from the local repo");
 		System.out.println("Our second commit from the local repository");
 		System.out.println("Now I'm on my togan branch");
+		System.out.println("I want to have conflict");
+		
+		System.out.println("I want to have a second conflict");
 	}
 }
