f13f9125011f6d63115ff01f56d2c76732e4e1ca
Meryem Togan <meryemtogan6@gmail.com>
2020-08-15 17:44:53 +0200

    My local togan branch

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from the local repo");
 		System.out.println("Our second commit from the local repository");
+		System.out.println("Now I'm on my togan branch");
 	}
 }
